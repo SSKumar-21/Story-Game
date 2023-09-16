6ffc9a7e0f9987d2754f7e4a08547c6fd7d42848
SIDHARTH KUMAR RATHORE <141614109+SSKumar-21@users.noreply.github.com>
2023-09-16 04:33:24 -0400

    Update index.js
    remove

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const audioElement = document.createElement('audio');
-    audioElement.autoplay = true; 
-    const mp3Source = document.createElement('source');
-    mp3Source.src = 'happy.mp3';
-    mp3Source.type = 'audio/mpeg';
-    audioElement.appendChild(mp3Source);
-    document.body.appendChild(audioElement);
-});
+console.log("This is the game home page");
